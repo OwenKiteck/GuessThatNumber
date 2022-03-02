8b2431214bdf81afdc394108ac8942263cf31f6a
Student <owen.kiteck@stu.barren.kyschools.us>
2022-03-01 19:11:28 -0600

    finished number guessing project. (added a range tracker for the player)

--- script.js
+++ script.js
@@ -16,7 +16,9 @@ alert(`Welcome to "Guess That Number"! press OK to start`)
 
 
 while (restartGame) {
-    // create range
+    topRangeNum = undefined;
+    lowRangeNum = 0;
+    // Set range
     rangeNum = parseInt(prompt(`What is the range you want?`));
     rangeNum = parseInt(rangeNum)
     // This is that if the number is negative, zero, or text, it will ask for it again 
@@ -26,70 +28,56 @@ while (restartGame) {
     topRangeNum = rangeNum;
 
     // generate random number
-    // randomNum = Math.floor(Math.random() * rangeNum + 1);
-    randomNum = Math.random();
-    randomNum *= rangeNum;
-    randomNum += 1;
-    randomNum = Math.floor(randomNum)
+    randomNum = Math.floor(Math.random() * rangeNum + 1);
 
     // Set the number of attempts
     attempts = parseInt(prompt(`Please enter a number of attempts:`));
-    while (!attempts || attempts < 1 || attempts > randomNum) {
+    while (!attempts || attempts < 1 || attempts >= rangeNum) {
         attempts = parseInt(prompt(`Attempts must be a number between 0 and ${rangeNum}`));
     };
 
-    
-
-
-    // 
-    // 
-    alert(`hahaha`)
-
+    // Actual game play
     guess = prompt(`Pick a number from 0 to ${rangeNum}. You have ${attempts} attempt(s) left:`)
-    while (true) {
-        guess = parseInt(guess)
-        while (!guess || guess < 1 || guess > rangeNum) {
-            guess = parseInt(prompt(`Pick a number from 0 to ${rangeNum}. You have ${attempts} attempt(s) left:`))
+        while (true) {
+            if (guess == `fries with that`) {
+                alert(`The number is ${randomNum}`);
+                guess = prompt(`Pick a number from ${lowRangeNum} to ${topRangeNum}. You have ${attempts} attempt(s) left:`)
+            }
+            guess = parseInt(guess)
+            while (!guess || guess < 1 || guess > rangeNum) {
+                guess = parseInt(prompt(`Pick a number from 0 to ${rangeNum}. You have ${attempts} attempt(s) left:`))
+            }
+            attempts--;
+
+            if (guess === randomNum) {
+                alert(`CONGRATULATIONS YOU GUESSED THE CORRECT NUMBER: ${randomNum}`)
+                break;
+            } else if (attempts === 0) {
+                alert(`Sorry, but tou ran out of attempts. The number was ${randomNum}`)
+                break;
+            } else if (guess < randomNum) {
+                if (guess > lowRangeNum) {
+                    lowRangeNum = parseInt(guess);
+                };
+                guess = prompt(`Too low. Pick a number from ${lowRangeNum} to ${topRangeNum}. You have ${attempts} attempt(s) left:`)
+            } else if (guess > randomNum) {
+                if (guess < topRangeNum) {
+                    topRangeNum = parseInt(guess);
+                };
+                guess = prompt(`Too high. Pick a number from ${lowRangeNum} to ${topRangeNum}. You have ${attempts} attempt(s) left:`)
+            };
         }
-        attempts--;
 
-        if (guess === randomNum) {
-            alert(`CONGRATULATIONS YOU GUESSED THE CORRECT NUMBER: ${randomNum}`)
+        playAgain = prompt(`Would you like to play again? Y for yes. N for no.`)
+        while (true) {
+        if (playAgain.toUpperCase() === `N`) {
+            alert( `Thanks for playing!`)
+            restartGame = false;
             break;
-        } else if (attempts === 0) {
-            alert(`Sorry, but tou ran out of attempts. The number was ${randomNum}`)
+        } else if (playAgain.toUpperCase() === `Y`) {
             break;
-        } else if (guess < randomNum) {
-            guess = prompt(`Too low. Pick a number from 0 to ${rangeNum}. You have ${attempts} attempt(s) left:`)
-        } else if (guess > randomNum) {
-            guess = prompt(`Too high. Pick a number from 0 to ${rangeNum}. You have ${attempts} attempt(s) left:`)
-
+        } else {
+            playAgain = prompt(`Please enter Y or N.`)
         }
     }
-    // 
-    // 
-
-    break;
 }
-
-            // Entering guesses
-                guess = undefined;
-                while (guess !== randomNum) {
-
-                    if (guess == undefined) {
-                        guess = parseInt(prompt(`Pick a number from 0 to ${rangeNum}. You have ${attempts} attempt(s) left:`));
-                    }
-
-                    if (guess > randomNum) {
-                        guess = parseInt(prompt(`Too high. Pick a number from ${lowRangeNum} to ${topRangeNum}. You have ${attempts} attempt(s) left:`));
-                    } else if (guess < randomNum) {
-                        guess = parseInt(prompt(`Too low. Pick a number from ${lowRangeNum} to ${topRangeNum}. You have ${attempts} attempt(s) left:`));
-                    };
-                    // Changing the seen range in the prompts (not the actual range)
-                    if (guess < topRangeNum && guess > rangeNum) {
-                        topRangeNum = guess;
-                    } 
-                    if (guess > lowRangeNum && guess < rangeNum) {
-                        lowRangeNum = guess;
-                    }
-                }
